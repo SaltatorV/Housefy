6f56a4946f056cba3251d1a7538e783ecfc5591d
Saltator <saltatorv@gmail.com>
2024-10-13 11:28:15 +0200

    note-shelf - chore: init module

---
+++ note-shelf/src/main/java/com/saltator/housefy/noteshelf/NoteShelfApplication.java
@@ -0,0 +1,13 @@
+package com.saltator.housefy.noteshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteShelfApplication.class, args);
+    }
+
+}
