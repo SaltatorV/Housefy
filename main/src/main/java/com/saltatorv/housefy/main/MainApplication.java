03a3f3c253719eaa9ed5cacd1f9fac33fad84b8e
Saltator <saltatorv@gmail.com>
2024-10-15 22:04:16 +0200

    main - chore: init module

---
+++ main/src/main/java/com/saltatorv/housefy/main/MainApplication.java
@@ -0,0 +1,21 @@
+package com.saltatorv.housefy.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.saltatorv")
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:note-shelf/application.properties")
+})
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
