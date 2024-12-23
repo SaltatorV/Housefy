e3cc6c5b19a666e36a6fd3bb35ff82379d05176f
Saltator <saltatorv@gmail.com>
2024-12-23 23:19:06 +0100

    [3]:feat - create Destination VO

---
+++ file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/Destination.java
@@ -0,0 +1,16 @@
+package com.saltatorv.file.storage.manager;
+
+import java.nio.file.Path;
+
+class Destination {
+    private final Path destination;
+
+    Destination(String destination) {
+        this.destination = Path.of(destination);
+    }
+
+    Path resolve(String fileName) {
+        return destination.resolve(fileName);
+    }
+
+}
---
+++ file-storage-manager/src/test/java/com/saltatorv/file/storage/manager/DestinationTest.java
@@ -0,0 +1,56 @@
+package com.saltatorv.file.storage.manager;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DestinationTest {
+
+    Destination destination;
+    Path resultPath;
+
+    @Test
+    @DisplayName("Can resolve absolute destination properly")
+    public void canResolveAbsoluteDestinationProperly() {
+        // given
+        createDestination("/tmp");
+
+        // when
+        resolve("test.txt");
+
+        // then
+        assertPathIsEqualTo("/tmp/test.txt");
+
+    }
+
+    @Test
+    @DisplayName("Can resolve relative destination properly")
+    public void canResolveRelativeDestinationProperly() {
+        // given
+        createDestination("tmp");
+
+        // when
+        resolve("test.txt");
+
+        // then
+        assertPathIsEqualTo("tmp/test.txt");
+
+    }
+
+    private void createDestination(String destination) {
+        this.destination = new Destination(destination);
+    }
+
+    private void resolve(String fileName) {
+        this.resultPath = destination.resolve(fileName);
+    }
+
+    private void assertPathIsEqualTo(String expectedPath) {
+        expectedPath = expectedPath.replace("/", java.io.File.separator);
+        expectedPath = expectedPath.replace("\"", java.io.File.separator);
+        assertEquals(expectedPath, resultPath.toString());
+    }
+}
