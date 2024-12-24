ba9986178fffa9a6aed91a3f8a6650a2e21b0105
Saltator <saltatorv@gmail.com>
2024-12-24 12:21:57 +0100

    [3]:test - remove object mother

--- file-storage-manager/src/test/java/com/saltatorv/file/storage/manager/UploadFileCommandObjectMother.java
+++
@@ -1,8 +0,0 @@
-package com.saltatorv.file.storage.manager;
-
-class UploadFileCommandObjectMother {
-
-    static UploadFileCommand createTextFileWithTestContentInTempDirectoryCommand() {
-        return new UploadFileCommand("test.txt", "temp", "Test content".getBytes(), true);
-    }
-}
