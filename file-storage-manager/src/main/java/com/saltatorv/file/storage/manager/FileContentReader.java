4ff0002795502c0b11fa7a1dae90a858fd1973c7
Saltator <saltatorv@gmail.com>
2024-12-26 22:51:41 +0100

    [3]:chore - update import to Destination in FileContentReader

--- file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/FileContentReader.java
+++ file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/FileContentReader.java
@@ -1,5 +1,7 @@
 package com.saltatorv.file.storage.manager;
 
+import com.saltatorv.file.storage.manager.VO.Destination;
+
 public interface FileContentReader<T> {
     T read(Destination fileDestination);
 }
