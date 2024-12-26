720656c9fbc5c9bb6ddbed446a83c939c08303d0
Saltator <saltatorv@gmail.com>
2024-12-26 23:55:23 +0100

    [3]:chore - add lombok @Getter annotation to UploadFileCommand

--- file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/command/UploadFileCommand.java
+++ file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/command/UploadFileCommand.java
@@ -1,11 +1,14 @@
 package com.saltatorv.file.storage.manager.command;
 
 import com.saltatorv.file.storage.manager.vo.Destination;
+import lombok.Getter;
 
 public class UploadFileCommand {
     private final String fileName;
+    @Getter
     private final Destination destination;
     private final byte[] content;
+    @Getter
     private final boolean createDirectories;
 
     public UploadFileCommand(String fileName, String destination, byte[] content, boolean createDirectories) {
@@ -19,15 +22,8 @@ public class UploadFileCommand {
         return destination.resolve(fileName);
     }
 
-    public Destination getDestination() {
-        return destination;
-    }
-
     public byte[] getContent() {
         return content;
     }
 
-    public boolean isCreateDirectories() {
-        return createDirectories;
-    }
 }
