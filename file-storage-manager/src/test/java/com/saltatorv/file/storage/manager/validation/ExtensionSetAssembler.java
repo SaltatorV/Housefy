d92a49ba4825d4e5f6b728edde121ee5d4e4713d
Saltator <saltatorv@gmail.com>
2024-12-28 23:52:14 +0100

    [3]:test - change ExtensionListAssembler to assemble extensions to set instead of list

---
+++ file-storage-manager/src/test/java/com/saltatorv/file/storage/manager/validation/ExtensionSetAssembler.java
@@ -0,0 +1,29 @@
+package com.saltatorv.file.storage.manager.validation;
+
+import com.saltatorv.file.storage.manager.vo.Extension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExtensionSetAssembler {
+
+    private Set<Extension> extensions;
+
+    private ExtensionSetAssembler() {
+        extensions = new HashSet<>();
+    }
+
+    public static ExtensionSetAssembler buildExtensions() {
+        return new ExtensionSetAssembler();
+    }
+
+    public ExtensionSetAssembler addExtension(String extension) {
+        extensions.add(new Extension(extension));
+        return this;
+    }
+
+    public Set<Extension> create() {
+        return extensions;
+    }
+
+}
