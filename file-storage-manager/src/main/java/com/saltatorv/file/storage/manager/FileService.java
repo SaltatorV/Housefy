b6b3d01bc6a2ad3e78d2e3f0a70f148df2ea9f55
Saltator <saltatorv@gmail.com>
2025-01-01 10:49:36 +0100

    [3]:feat - FileService add methods headers

--- file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/FileService.java
+++ file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/FileService.java
@@ -1,4 +1,13 @@
 package com.saltatorv.file.storage.manager;
 
+import com.saltatorv.file.storage.manager.command.UploadFileCommand;
+
+import java.nio.file.Path;
+import java.util.List;
+
 public interface FileService {
+
+    List<File> getFilesFrom(Path directoryPath);
+    File getFile(Path filePath);
+    File uploadFile(UploadFileCommand uploadFileCommand);
 }
