9b4220e24d97dbcd8230db5b34a8c7d6519553fa
Saltator <saltatorv@gmail.com>
2025-01-02 22:35:44 +0100

    [3]:feat - implement other FileServiceImpl methods

--- file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/FileServiceImpl.java
+++ file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/FileServiceImpl.java
@@ -4,17 +4,24 @@ import com.saltatorv.file.storage.manager.command.UploadFileCommand;
 import com.saltatorv.file.storage.manager.validation.FileValidationRule;
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
-public class FileServiceImpl implements FileService{
+public class FileServiceImpl implements FileService {
 
     private final FileValidationRule validationRule;
 
     @Override
     public List<File> getFiles(Path directoryPath) {
-        return null;
+        try (Stream<Path> filePaths = Files.list(directoryPath)) {
+            return filePaths.map(File::new).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- file-storage-manager/src/test/java/com/saltatorv/file/storage/manager/FileServiceImplTest.java
+++ file-storage-manager/src/test/java/com/saltatorv/file/storage/manager/FileServiceImplTest.java
@@ -8,10 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.saltatorv.file.storage.manager.command.UploadFileCommandAssembler.buildUploadFileCommand;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 
@@ -44,16 +48,70 @@ public class FileServiceImplTest extends FilesBasedTest {
         assertEquals(expectedFile, actualFile);
     }
 
+    @Test
+    @DisplayName("Can upload new file")
+    public void canUploadNewFile() {
+        //given
+        var fileName = "test.txt";
+        var command = createUploadFileCommand(fileName);
+
+        //when
+        var uploadedFile = uploadFile(command);
+
+        //then
+        assertFileExists(fileName);
+    }
+
+    @Test
+    @DisplayName("Can get list of files from specific directory")
+    public void canGetListOfFilesFromSpecificDirectory() {
+        //given
+        var expectedFiles = createTenTextFiles();
+
+        //when
+        var actualFiles = getFiles();
+
+        //then
+        assertEquals(expectedFiles, actualFiles);
+    }
+
+
+    private File uploadFile(UploadFileCommand command) {
+        return fileService.uploadFile(command);
+    }
+
+
     private File getFile(String fileName) {
         return fileService.getFile(TEST_DIRECTORY.resolve(fileName));
     }
 
+    private List<File> getFiles() {
+        return fileService.getFiles(TEST_DIRECTORY);
+    }
+
     private File createTestTextFile(String fileName) {
-        UploadFileCommand command = buildUploadFileCommand()
+        UploadFileCommand command = createUploadFileCommand(fileName);
+
+        return File.upload(command, mock(FileValidationRule.class));
+    }
+
+    private UploadFileCommand createUploadFileCommand(String fileName) {
+        return buildUploadFileCommand()
                 .uploadTextFileAsDefault()
                 .butWithFileName(TEST_DIRECTORY.resolve(fileName))
                 .create();
+    }
 
-        return File.upload(command, mock(FileValidationRule.class));
+    private List<File> createTenTextFiles() {
+        List<File> files = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            File file = createTestTextFile(String.join("", "test", String.valueOf(i), ".txt"));
+            files.add(file);
+        }
+        return files;
+    }
+
+    private void assertFileExists(String fileDestination) {
+        assertTrue(Files.exists(TEST_DIRECTORY.resolve(fileDestination)));
     }
 }
