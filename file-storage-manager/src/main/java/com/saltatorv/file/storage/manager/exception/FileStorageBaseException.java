66d39192f1e317be94933f5fd3c8a1afde076221
Saltator <saltatorv@gmail.com>
2025-01-05 22:59:09 +0100

    [3]:feat - FileStorageBaseException change access modifier to public, add additional constructor

--- file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/exception/FileStorageBaseException.java
+++ file-storage-manager/src/main/java/com/saltatorv/file/storage/manager/exception/FileStorageBaseException.java
@@ -1,7 +1,11 @@
 package com.saltatorv.file.storage.manager.exception;
 
-class FileStorageBaseException extends RuntimeException {
+public class FileStorageBaseException extends RuntimeException {
     public FileStorageBaseException(String message) {
         super(message);
+    }
+
+    public FileStorageBaseException(Throwable cause) {
+        super(cause);
     }
 }
