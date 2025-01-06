c74af223f3d19928a980d36bf9ac6ea0ecf6d97e
Saltator <saltatorv@gmail.com>
2025-01-06 17:49:28 +0100

    [4]:chore - separate billing manager to container, application, domain and dataaccess submodules

---
+++ billing-manager/billing-manager-container/src/main/java/com/example/billing/manage/rcontainer/BillingManagerContainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.billing.manage.rcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingManagerContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingManagerContainerApplication.class, args);
+    }
+
+}
